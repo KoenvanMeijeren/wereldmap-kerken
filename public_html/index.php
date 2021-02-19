a49a4e4491992e02163f92cbc7a4ecc0573879de
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-02-19 17:24:17 +0100

    Add clustered markers

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Wereldmap kerken</title>
@@ -19,11 +19,6 @@
             bottom: 0;
             width: 100%;
         }
-
-        .mapboxgl-popup {
-            max-width: 400px;
-            font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
-        }
     </style>
 </head>
 <body>
@@ -40,162 +35,223 @@
     });
 
     map.on('load', function () {
-        map.loadImage(
-            'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-            // Add an image to use as a custom marker
-            function (error, image) {
-                if (error) throw error;
-                map.addImage('custom-marker', image);
-
-                map.addSource('places', {
-                    'type': 'geojson',
-                    'data': {
-                        'type': 'FeatureCollection',
-                        'features': [
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.038659, 38.931567]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.003168, 38.894651]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.090372, 38.881189]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.111561, 38.882342]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.052477, 38.943951]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.043444, 38.909664]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.031706, 38.914581]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.020945, 38.878241]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Kerk A</strong><br><a href="www.google.com">Kerk</a>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.007481, 38.876516]
-                                }
-                            }
-                        ]
+        // Add a new source from our GeoJSON data and
+        // set the 'cluster' option to true. GL-JS will
+        // add the point_count property to your source data.
+        map.addSource('kerken', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "ak16994521",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.038659, 38.931567]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "ak16994519",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.003168, 38.894651]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "ak16994517",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.090372, 38.881189]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "ci38021336",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.111561, 38.882342]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "us2000b2nn",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.052477, 38.943951]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "ak16994510",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.043444, 38.909664]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "us2000b2nb",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.031706, 38.914581]
+                        }
+                    },
+                    {
+                        'type': 'Feature',
+                        'properties': {
+                            "id": "nc72905861",
+                            "title": "Kerk a",
+                            "link": 'www.google.com'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-77.020945, 38.878241]
+                        }
                     }
-                });
-
-                // Add a layer showing the places.
-                map.addLayer({
-                    'id': 'places',
-                    'type': 'symbol',
-                    'source': 'places',
-                    'layout': {
-                        'icon-image': 'custom-marker',
-                        'icon-allow-overlap': true
-                    }
-                });
-            }
-        );
-
-        // Create a popup, but don't add it to the map yet.
-        const popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
+                ]
+            },
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
         });
 
-        map.on('mouseenter', 'places', function (e) {
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
+        map.addLayer({
+            id: 'clusters',
+            type: 'circle',
+            source: 'kerken',
+            filter: ['has', 'point_count'],
+            paint: {
+                // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+                // with three steps to implement three types of circles:
+                //   * Blue, 20px circles when point count is less than 100
+                //   * Yellow, 30px circles when point count is between 100 and 750
+                //   * Pink, 40px circles when point count is greater than or equal to 750
+                'circle-color': [
+                    'step',
+                    ['get', 'point_count'],
+                    '#51bbd6',
+                    100,
+                    '#f1f075',
+                    750,
+                    '#f28cb1'
+                ],
+                'circle-radius': [
+                    'step',
+                    ['get', 'point_count'],
+                    20,
+                    100,
+                    30,
+                    750,
+                    40
+                ]
+            }
+        });
 
+        map.addLayer({
+            id: 'cluster-count',
+            type: 'symbol',
+            source: 'kerken',
+            filter: ['has', 'point_count'],
+            layout: {
+                'text-field': '{point_count_abbreviated}',
+                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                'text-size': 12
+            }
+        });
+
+        map.addLayer({
+            id: 'unclustered-point',
+            type: 'circle',
+            source: 'kerken',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+                'circle-color': '#11b4da',
+                'circle-radius': 10,
+                'circle-stroke-width': 3,
+                'circle-stroke-color': '#fff'
+            }
+        });
+
+        // inspect a cluster on click
+        map.on('click', 'clusters', function (e) {
+            var features = map.queryRenderedFeatures(e.point, {
+                layers: ['clusters']
+            });
+            var clusterId = features[0].properties.cluster_id;
+            map.getSource('kerken').getClusterExpansionZoom(
+                clusterId,
+                function (err, zoom) {
+                    if (err) return;
+
+                    map.easeTo({
+                        center: features[0].geometry.coordinates,
+                        zoom: zoom
+                    });
+                }
+            );
+        });
+
+        // When a click event occurs on a feature in
+        // the unclustered-point layer, open a popup at
+        // the location of the feature, with
+        // description HTML from its properties.
+        map.on('click', 'unclustered-point', function (e) {
             const coordinates = e.features[0].geometry.coordinates.slice();
-            const description = e.features[0].properties.description;
+            const title = e.features[0].properties.title;
+            const link = e.features[0].properties.link;
 
-            // Ensure that if the map is zoomed out such that multiple
-            // copies of the feature are visible, the popup appears
-            // over the copy being pointed to.
+            // Ensure that if the map is zoomed out such that
+            // multiple copies of the feature are visible, the
+            // popup appears over the copy being pointed to.
             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
             }
 
-            // Populate the popup and set its coordinates
-            // based on the feature found.
-            popup.setLngLat(coordinates).setHTML(description).addTo(map);
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(
+                    'Title: ' + title + '<br><a href="' + link + '">' + title + '</a>'
+                )
+                .addTo(map);
         });
 
-        map.on('mouseleave', 'places', function () {
+        map.on('mouseenter', 'clusters', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+
+        map.on('mouseleave', 'clusters', function () {
             map.getCanvas().style.cursor = '';
-            popup.remove();
         });
     });
 </script>
